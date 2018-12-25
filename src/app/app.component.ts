5fd2e8bbbcb5d8aa798c36ce6f5bc00860ed715b
andreyfoggy <andreyfoggy9009@gmail.com>
2018-12-25 22:04:41 +0200

    scroll fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {standartBase} from './register/standartBaseUser';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Sun CINEMA';
-  constructor() {
+  constructor(private router: Router) {
     localStorage.setItem('userBase', JSON.stringify(standartBase));
     localStorage.setItem('userLogin', 'off');
+  }
+
+  ngOnInit() {
+    this.router.events.subscribe((evt) => {
+      if (!(evt instanceof NavigationEnd)) {
+          return;
+      }
+      window.scrollTo(0, 0);
+  });
   }
 }
