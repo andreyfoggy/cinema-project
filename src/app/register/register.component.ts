989adea75ee71aef1fb72bda40648f0a4fd80e99
HalimovTimurhan <halimovtimurhan@gmail.com>
2018-12-26 22:28:58 +0200

    css rewrite register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -125,12 +125,12 @@ export class RegisterComponent implements OnInit {
   }
 
   public loadScript() {
-    const node = document.createElement('script');
-    node.src = './src/app/register/snow.js';
-    node.type = 'text/javascript';
-    node.async = true;
-    node.charset = 'utf-8';
-    // document.getElementsByTagName('body')[0].appendChild(node);
-    document.getElementsByClassName('registerContent')[0].appendChild(node);
+    // const node = document.createElement('script');
+    // node.src = './src/app/register/snow.js';
+    // node.type = 'text/javascript';
+    // node.async = true;
+    // node.charset = 'utf-8';
+    // // document.getElementsByTagName('body')[0].appendChild(node);
+    // document.getElementsByClassName('registerContent')[0].appendChild(node);
   }
 }
